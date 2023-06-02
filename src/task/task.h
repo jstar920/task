f98eb9b99439b9d9f8060f71f6d67fa7964e5a8d
jianxjia <jianxjia@cisco.com>
2023-06-02 19:47:18 +0800

    add task

--- src/task/task.h
+++ src/task/task.h
@@ -1,3 +1,21 @@
+#include <functional>
 struct Task
 {
+    Task() = delete;
+    template<typename R, typename... Arg>
+    Task(const std::function<R(const Arg&...)>& f, const Arg&... arg)
+    {
+        if (f) {
+            func = [f, arg...]() {
+                f(arg...);
+            };
+        }
+    }
+
+    void operator()()
+    {
+        if (func)
+           func();
+    }
+    std::function<void()> func;
 };
